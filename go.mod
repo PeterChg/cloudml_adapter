f008bc6ef62e561c54414be37fc1827ec2aef073
Your Name <you@example.com>
2020-11-16 21:01:49 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module cloudml_adapter
+
+go 1.15
+
+require (
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/apimachinery v0.18.0
+	k8s.io/client-go v0.18.0
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
+)
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/woainizhongguo/cloudml_adapter/pkg/cloudml_gpu_adapter"
+	"fmt"
+	_ "github.com/woainizhongguo/cloudml_apaptercloudml_adapter/pkg/cloudml_gpu_adapter"
+)
+
+var d = cloudml_gpu_adapter.GetGpuResourceSock()
+
+func main()  {
+	fmt.Println(d)
+	fmt.Println(cloudml_gpu_adapter.GetGpuResourceName())
+}
